02cf0b16c2811a3e6f0ff849811bb76a1f5f13f5
idriosiris <idriceanu.stefan@gmail.com>
2021-06-09 20:11:48 +0100

    Store arabic to roman mappings

--- roman-numerals/src/main/java/com/example/romannumerals/ArabicNumbersTranslator.java
+++ roman-numerals/src/main/java/com/example/romannumerals/ArabicNumbersTranslator.java
@@ -1,13 +1,28 @@
 package com.example.romannumerals;
 
+import java.util.HashMap;
+
 public class ArabicNumbersTranslator {
+
+    private final HashMap<Integer, String> romanNumbersMap;
+
+    public ArabicNumbersTranslator() {
+        romanNumbersMap = new HashMap<>();
+
+        romanNumbersMap.put(1, "I");
+    }
+
     String roman(int i) {
         if(i == 1)
-            return "I";
+            return getI(1);
 
         if(i == 2)
-            return "II";
+            return getI(1) + getI(1);
 
         return null;
+    }
+
+    private String getI(int i) {
+        return romanNumbersMap.get(i);
     }
 }
